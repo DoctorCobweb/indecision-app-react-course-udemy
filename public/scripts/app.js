de404133e6b7ef918dd913bc1cc1807e6547ce0b
dre <spinninghalf@gmail.com>
2018-08-12 19:05:17 +1000

    init

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+console.log('app.js is running');
+
+// JSX - Javascript XML
+var template = <p>this is JSX from app.js</p>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot );
